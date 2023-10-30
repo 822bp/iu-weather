fbe1fdd1822bb4f77eb36be8978fb75690fd0231
benjamin.pascale <benjamin.pascale@hoffmann-group.com>
2023-10-30 16:13:57 +0100

    added location state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import React, { useState } from 'react';
+import './style.css';
+
+function Location() {
+  const [pos, setPos] = useState("");
+  const geolocator = navigator.geolocation.getCurrentPosition(setPos);
+  return (
+    <div className='location'>{(pos && (pos.coords.latitude + ", " + pos.coords.longitude)) || "Getting Location..."}</div>
+  )
+}
+
+function header() {
+  return (
+    <div className="header">{Location()}</div>
+  )
+}
+
 function App() {
   return (
-    <div className="App">
-      <p>hi</p>
+    <div>
+      {header()}
     </div>
   );
 }
