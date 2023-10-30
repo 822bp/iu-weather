496ccddc00f28b462c4ee6ece5c5741af36841db
benjamin.pascale <benjamin.pascale@hoffmann-group.com>
2023-10-30 16:45:09 +0100

    refactored & added api call

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 
-function Location() {
-  const [pos, setPos] = useState("");
-  const geolocator = navigator.geolocation.getCurrentPosition(setPos);
+function Location(pos) {
   return (
     <div className='location'>{(pos && (pos.coords.latitude + ", " + pos.coords.longitude)) || "Getting Location..."}</div>
   )
 }
 
-function header() {
-  return (
-    <div className="header">{Location()}</div>
-  )
-}
-
 function App() {
+  const [position, setPos] = useState("");
+  const geolocator = navigator.geolocation.getCurrentPosition(setPos);
+
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    /*position && fetch("https://api.openweathermap.org/data/2.5/weather?lat="+position.coords.latitude+"&lon="+position.coords.longitude+"&appid="+process.env.OW_API_KEY)
+    .then((res) => setData(res))*/
+    position && console.log(position.coords.longitude)
+    return;
+  }, [position]);
+
+  console.log(data);
+
   return (
     <div>
-      {header()}
+      <div className='header'>
+        {Location(position)}
+      </div>
     </div>
   );
 }
